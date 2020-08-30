e645cd0579980723389aa30933bae9141fb5df18
pawan singhla <pawansinghla300@gmail.com>
2020-08-30 20:24:20 +0530

    add client

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -5,12 +5,13 @@ const io=require('socket.io')(8000)
 const  users={};
 io.on('connection',socket=>{
     socket.on('new-user-joined',name=>{
+        console.log("NEW USER",name);
         users[socket.id]=name;
         socket.broadcast.emit('user-joined',name);
     });
 
     socket.on('send',message=>{
-        socket.broadcast.emit('receive',{message:message,name:user[socket.id]})
+        socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
     });
 })
 
