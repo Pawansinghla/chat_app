fdf4779c5a59cddd0b9ab750fa299a49629c8d50
pawan singhla <pawansinghla300@gmail.com>
2020-09-01 00:39:08 +0530

    final-update

--- js/client.js
+++ js/client.js
@@ -1,37 +1,55 @@
 const socket=io('http://localhost:8000');
 
+//Get DOM elements in respective js variables
 const form=document.getElementById('send-container');
 const messageInput=document.getElementById('messageInp');
 
 const messageContainer=document.querySelector(".container");
 
+//audio that will play on recieving msgs
+var audio =new Audio('../ring.mp3');
+
+//funtion which will append event info to the container
 const append=(message,position)=>{
     const messageElement=document.createElement('div');
     messageElement.innerText=message;
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
+    if(position=='left'){
+    audio.play();
+    }
 
 
 }
+
+//ask for new user his/her name and let the server
+const name=prompt("enter your name to join");
+socket.emit('new-user-joined',name);
+
+//if a new user joins, recive user name from the server
+socket.on('user-joined',name=>{
+    append(`${name} : joined the chat`,`right`);
+
+})
+
+//if server send a msgs, receive it
+socket.on('receive',data=>{
+    append(`${data.name} : ${data.message}`,`left`);
+
+})
+
+//if a user leave the chat, append the info to the conatiner
+socket.on('leave',name=>{
+    append(`${name}: left the chat`,`right`);
+
+});
+
+//if the form gets submitted send server the message
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
     const message=messageInput.value;
     append(`You: ${message}`,'right');
     socket.emit('send',message);
     messageInput.value='';
-})
-
-const name=prompt("enter your name to join");
-socket.emit('new-user-joined',name);
-
-socket.on('user-joined',name=>{
-    append(`${name} : joined the chat`,`right`);
-
-})
-
-
-socket.on('receive',data=>{
-    append(`${data.name} : ${data.message}`,`left`);
-
 })
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,17 +1,25 @@
 //Node Server which will handle socket io  connections
-
+//to start server use nodemon index.js
 const io=require('socket.io')(8000)
 
 const  users={};
 io.on('connection',socket=>{
+    //if any new user joins, let other users connected to the server know!
     socket.on('new-user-joined',name=>{
-        console.log("NEW USER",name);
+      //  console.log("NEW USER",name);
         users[socket.id]=name;
         socket.broadcast.emit('user-joined',name);
     });
 
+    //if someone send a message, braodtcast it to other people
     socket.on('send',message=>{
         socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
+    });
+//if someone leave the chat, let other know
+//desconnect is built-in event
+    socket.on('disconnect',message=>{
+        socket.broadcast.emit('leave',users[socket.id]);
+        delete users[socket.id]
     });
 })
 
